36d45b102a7dcdbaacf6ad3cd3d528cf988bfc05
najm <jemmeli84@gmail.com>
2021-09-04 12:27:56 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculgr20';
+
+  inputValue = 0;
+  private op = '';
+  private result = 0;
+  private myFloat = 0.0;
+
+  getOp( data : any ) {
+    console.log(data);
+    this.op = data;
+  }
+
+  getNumber( data : any ){
+
+    if( this.op == "+" ){
+      this.result = this.inputValue + Number(data);
+    }
+    if( this.op == "-" ){
+      this.result = this.inputValue - Number(data);
+    }
+    if( this.op == "/" ){
+      this.result = this.inputValue / Number(data);
+    }
+    if( this.op == "*" ){
+      this.result = this.inputValue * Number(data);
+    }
+    if( this.op == "." ){
+      this.inputValue = parseFloat(this.inputValue +'.'+ data);
+    }else{
+      this.inputValue = Number(data);
+    }
+    console.log(this.result);
+  }
+
+  reset(){
+    this.inputValue = 0;
+  }
+
+  calc(){
+    this.inputValue = this.result;
+  }
+
+
+
 }
