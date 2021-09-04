4746bbf68218062722b79c0bd927015dd2565133
najm <jemmeli84@gmail.com>
2021-09-04 17:31:25 +0100

    add color and enhance code calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,44 +7,82 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  inputValue = 0;
+  inputValue = '0';
   private op = '';
-  private result = 0;
-  private myFloat = 0.0;
+  private result : any;
+  private isOP = false;
+  color = '';
+
+ /*  getColorInput(data:any){
+    this.color = data;
+    console.log( data );
+  } */
+
+  getColor(color : any){
+    this.color = color;
+  }
 
   getOp( data : any ) {
     console.log(data);
     this.op = data;
+    this.isOP = true;
+  }
+
+  getP() {
+    if( !this.inputValue.includes('.') ){ this.inputValue = this.inputValue + '.' };
   }
 
   getNumber( data : any ){
 
-    if( this.op == "+" ){
-      this.result = this.inputValue + Number(data);
-    }
-    if( this.op == "-" ){
-      this.result = this.inputValue - Number(data);
-    }
-    if( this.op == "/" ){
-      this.result = this.inputValue / Number(data);
-    }
-    if( this.op == "*" ){
-      this.result = this.inputValue * Number(data);
-    }
-    if( this.op == "." ){
-      this.inputValue = parseFloat(this.inputValue +'.'+ data);
+
+
+    if(!this.isOP){
+
+      if( this.inputValue.includes('.') ){
+        this.inputValue += data;
+      }else{
+        this.inputValue = data;
+      }
+
     }else{
-      this.inputValue = Number(data);
+
+      this.result = this.calcul(this.op, this.inputValue, data);
+      this.inputValue = data;
+
     }
-    console.log(this.result);
+
+    this.isOP = false;
+
+    console.log('res ---->', this.result);
+  }
+
+
+
+  calcul( op:string , number1:string , number2:string ) : number {
+    if( op == "+" ){
+      return ( this.result = Number(number1) + Number(number2) );
+    }
+    if( op == "-" ){
+      return ( this.result = Number(number1) - Number(number2) );
+    }
+    if( op == "/" ){
+      return ( this.result = Number(number1) / Number(number2) );
+    }
+    if( op == "*" ){
+      return ( this.result = Number(number1) * Number(number2) );
+    }
+
+    return 0;
   }
 
   reset(){
-    this.inputValue = 0;
+    this.inputValue = '0';
+    this.isOP = false;
   }
 
   calc(){
     this.inputValue = this.result;
+    this.isOP = false;
   }
 
 
